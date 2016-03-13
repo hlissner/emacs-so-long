beb366fb34793bd5fc4b0e9e19f7fe792d09cf52
Phil Sainty <psainty@orcon.net.nz>
2016-03-13 20:24:56 +1300

    Handle header 'mode' declarations
    Hack local variables after reverting to the original major mode.
    
    Reverted `so-long-max-lines' to a default value of 5.

--- so-long.el
+++ so-long.el
@@ -5,7 +5,7 @@
 ;; Keywords: convenience
 ;; Created: 12 Jan 2016
 ;; Package-Requires: ((emacs "24.3"))
-;; Version: 0.6.5
+;; Version: 0.7
 
 ;; This file is not part of GNU Emacs.
 
@@ -70,22 +70,6 @@
 ;; the code for a given mode (on a case by case basis) to determine the right
 ;; way to disable it, as not all modes are alike.
 
-;; File-local MODE specifications
-;; ------------------------------
-;; Ideally we would defer seamlessly to any file-local MODE variable; but at
-;; present (Emacs 24.5) -*- mode: MODE; -*- header comments are processed by
-;; `set-auto-mode' directly, with the outcome that we never get a chance to
-;; inhibit our own mode switch.  Ultimately that specified mode *is* still
-;; called (as part of the main `hack-local-variables' evaluation), but our mode
-;; switch is *also* called prior to that, which is undesirable (as we display
-;; messages at that time).  Once Emacs drops the deprecated feature whereby
-;; 'mode:' is also allowed to specify minor-modes (i.e. there can be more than
-;; one "mode:"), this problem will be removed, as (hack-local-variables t) will
-;; handle file-local modes in all cases.
-;;
-;; In the interim, it's cleanest to use a Local Variables comment block to
-;; specify a mode override, if one is required.
-
 ;; Implementation notes
 ;; --------------------
 ;; This library advises `hack-local-variables' (in order that we may inhibit our
@@ -94,6 +78,9 @@
 
 ;;; Change Log:
 ;;
+;; 0.7   - Handle header 'mode' declarations.
+;;       - Hack local variables after reverting to the original major mode.
+;;       - Reverted `so-long-max-lines' to a default value of 5.
 ;; 0.6.5 - Inhibit globalized `hl-line-mode' and `whitespace-mode'.
 ;;       - Set `buffer-read-only' by default.
 ;; 0.6   - Added `so-long-minor-modes' and `so-long-hook'.
@@ -114,7 +101,7 @@
 
 See `so-long-line-detected-p' for details.")
 
-(defvar so-long-max-lines 20
+(defvar so-long-max-lines 5
   "Number of non-blank, non-comment lines to test for excessive length.
 
 See `so-long-line-detected-p' for details.")
@@ -160,6 +147,12 @@ See also `so-long-minor-modes'.")
 (defvar so-long-mode--inhibited nil) ; internal use
 (make-variable-buffer-local 'so-long-mode--inhibited)
 (put 'so-long-mode--inhibited 'permanent-local t)
+
+;; When the line's long
+;; When the mode's slow
+;; When Emacs is sad
+;; We change automatically to faster code
+;; And then I won't feel so mad
 
 (defun so-long-line-detected-p ()
   "Following any initial comments and blank lines, the next N lines of the
@@ -232,11 +225,13 @@ This happens during `after-change-major-mode-hook'."
 
 (defun so-long-mode-revert ()
   "Call the `major-mode' which was originally selected by `set-auto-mode'
-before `so-long-mode' was called to replace it."
+before `so-long-mode' was called to replace it, and then re-process the
+local variables."
   (interactive)
-  (if so-long-original-mode
-      (funcall so-long-original-mode)
-    (error "Original mode unknown.")))
+  (unless so-long-original-mode
+    (error "Original mode unknown."))
+  (funcall so-long-original-mode)
+  (hack-local-variables))
 
 (define-key so-long-mode-map (kbd "C-c C-c") 'so-long-mode-revert)
 
@@ -268,6 +263,60 @@ Run by default in `so-long-hook' to counteract `global-whitespace-mode'."
     ;; (setq-local whitespace-mode nil)
     ))
 
+(defun so-long-check-header-modes ()
+  "Handles the header-comments processing in `set-auto-mode'.
+
+`set-auto-mode' has some special-case code to handle the 'mode' pseudo-variable
+when set in the header comment.  This runs outside of `hack-local-variables'
+and cannot be conveniently intercepted, so we are forced to replicate it here.
+
+This special-case code will ultimately be removed from Emacs, as it exists to
+deal with a deprecated feature; but until then we need to replicate it in order
+to inhibit our own behaviour in the presence of a header comment 'mode'
+declaration."
+  ;; The following code for processing MODE declarations in the header
+  ;; comments is copied verbatim from `set-auto-mode', because we have
+  ;; no way of intercepting it.
+  ;;
+  (let ((try-locals (not (inhibit-local-variables-p)))
+        end done mode modes)
+    ;; Once we drop the deprecated feature where mode: is also allowed to
+    ;; specify minor-modes (ie, there can be more than one "mode:"), we can
+    ;; remove this section and just let (hack-local-variables t) handle it.
+    ;; Find a -*- mode tag.
+    (save-excursion
+      (goto-char (point-min))
+      (skip-chars-forward " \t\n")
+      ;; Note by design local-enable-local-variables does not matter here.
+      (and enable-local-variables
+           try-locals
+           (setq end (set-auto-mode-1))
+           (if (save-excursion (search-forward ":" end t))
+               ;; Find all specifications for the `mode:' variable
+               ;; and execute them left to right.
+               (while (let ((case-fold-search t))
+                        (or (and (looking-at "mode:")
+                                 (goto-char (match-end 0)))
+                            (re-search-forward "[ \t;]mode:" end t)))
+                 (skip-chars-forward " \t")
+                 (let ((beg (point)))
+                   (if (search-forward ";" end t)
+                       (forward-char -1)
+                     (goto-char end))
+                   (skip-chars-backward " \t")
+                   (push (intern (concat (downcase (buffer-substring beg (point))) "-mode"))
+                         modes)))
+             ;; Simple -*-MODE-*- case.
+             (push (intern (concat (downcase (buffer-substring (point) end))
+                                   "-mode"))
+                   modes))))
+
+    ;; `so-long' now processes the resulting mode list.  If any modes were
+    ;; listed, we assume that one of them is a major mode.  It's possible that
+    ;; this isn't true, but the buffer would remain in fundamental-mode if that
+    ;; were the case, so it is very unlikely.
+    (setq so-long-mode--inhibited modes)))
+
 ;; How do you solve a problem like a long line?
 ;; How do you stop a mode from slowing down?
 ;; How do you cope with processing a long line?
@@ -297,9 +346,15 @@ from `so-long-mode' (binary file modes, for example).  Instead, we act
 only when the selected major mode is a member (or derivative of a member)
 of `so-long-target-modes'.
 
-`so-long-line-detected-p' then determines whether the mode change is needed."
+`so-long-line-detected-p' then determines whether the mode change is needed.
+
+Local variables are not processed after changing to `so-long-mode', as
+they might negatively affect performance.  (Local variables are processed
+again if `so-long-mode-revert' is called, however.)"
   (setq so-long-mode--inhibited nil) ; is permanent-local
-  ad-do-it ; `set-auto-mode'
+  (when so-long-mode-enabled
+    (so-long-check-header-modes)) ; may set `so-long-mode--inhibited'
+  ad-do-it ; `set-auto-mode'      ; may set `so-long-mode--inhibited'
   (when so-long-mode-enabled
     (unless so-long-mode--inhibited
       (when (and (apply 'derived-mode-p so-long-target-modes)
